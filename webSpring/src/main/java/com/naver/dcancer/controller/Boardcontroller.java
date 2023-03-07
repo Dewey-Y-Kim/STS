f4f456d9b0d71720b8778af244124e1ed097bfac
youth <dcancer17@gmail.com>
2023-03-07 17:58:23 +0900

    230307

--- webSpring/src/main/java/com/naver/dcancer/controller/Boardcontroller.java
+++ webSpring/src/main/java/com/naver/dcancer/controller/Boardcontroller.java
@@ -1,8 +1,17 @@
 package com.naver.dcancer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-// Controller : veiw 언어 사용 불가-jsp생성 및 구현/ RestController : view 언어 사용 가능
+import org.springframework.web.servlet.ModelAndView;
+// Controller : veiw 언어 사용 불가-jsp생성 및 구현/ RestController : view 언어 사용 가능, 반환형 String = 컨텐츠
 @RestController
 public class Boardcontroller {
-	
+	@GetMapping("/board/list")
+	public ModelAndView boardlist() {
+		//db 조회
+		
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("/board/list");
+		return mav;
+	}
 }
